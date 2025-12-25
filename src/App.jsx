7fb16d77d7ad93b8b6c3a159de212d1ba94787db
Sabbir Hossain <sm.sabbir9671@gmail.com>
2025-12-25 22:29:28 +0600

    batsman history of cricket

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import './App.css'
 import Counter from './Counter'
-
+import Batsman from './Batsman'
 
 function App() {
-  
-
   return (
     <>
       <h1>Vite + React</h1>
+      <Batsman></Batsman>
       <Counter></Counter>
     </>
   )
---
+++ src/Batsman.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+export default function Batsman() {
+  const [runs, setRuns] = useState(0);
+  const[sixes , setSixes]= useState(0);
+  const[four, setFour]= useState(0);
+  const [single, setSingle] = useState(0);
+
+  const handleSingles = () => {
+    const updatedRuns = runs + 1;
+    const updatedSingles = single + 1;
+    setSingle(updatedSingles)
+    setRuns(updatedRuns);
+  };
+  const handleFour = () => {
+    const updatedRuns = runs + 4;
+    const updatedFour = four + 1;
+    setFour(updatedFour)
+    setRuns(updatedRuns);
+  };
+  const handleSix = () => {
+    const updatesRuns = runs + 6;
+    const updatedSixes = sixes + 1;
+    setSixes(updatedSixes)
+    setRuns(updatesRuns)
+  }
+
+  return (
+    <div>
+      <h2>Player: Bangla Batsman</h2>
+      <p><small>Six: {sixes}  Four: {four} Singles: {single}</small></p>
+
+      {
+        runs > 50 && <p>Your score is : 50</p>
+      }
+      <h2>Score: {runs}</h2>
+      <button onClick={handleSingles}>Singles</button>
+      <button onClick={handleFour}>Four</button>
+      <button onClick={handleSix}>Six</button>
+    </div>
+  );
+}
